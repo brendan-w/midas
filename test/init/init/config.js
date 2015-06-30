7fc8d53de21cc40f9d65a193fafb722a3df83cdf
Brendan Whitfield <brendanw@windworksdesign.com>
2015-06-30 15:29:18 -0400

    added an initial list of skill tags

--- test/init/init/config.js
+++ test/init/init/config.js
@@ -4,19 +4,30 @@ module.exports = {
     password: 'initializeI1!'
   },
   tags: [
+
+    /*
+      task-time-required
+    */
     {
       type: 'task-time-required',
-      name: 'Part Time'
+      name:'Full-time (40 hours)'
     },
     {
       type: 'task-time-required',
-      name: 'Full Time'
+      name:'Part-time (10-20 hours)'
     },
     {
       type: 'task-time-required',
-      name: '20% Time'
+      name:'Quick turnover'
     },
-    // ------------
+    {
+      type: 'task-time-required',
+      name:'Volunteer/Pro bono'
+    },
+
+    /*
+      task-people
+    */
     {
       type: 'task-people',
       name: '1 person'
@@ -33,7 +44,10 @@ module.exports = {
       type: 'task-people',
       name: 'A Team'
     },
-    // ------------
+
+    /*
+      task-skills-required
+    */
     {
       type: 'task-skills-required',
       name: 'Required'
@@ -42,7 +56,10 @@ module.exports = {
       type: 'task-skills-required',
       name: 'Not Required'
     },
-    // ------------
+
+    /*
+      task-length
+    */
     {
       type: 'task-length',
       name: '1 Day'
@@ -63,7 +80,10 @@ module.exports = {
       type: 'task-length',
       name: 'A Few Months'
     },
-    // -------------
+
+    /*
+      task-time-estimate
+    */
     {
       type: 'task-time-estimate',
       name: 'Less than 1 hour'
@@ -87,6 +107,258 @@ module.exports = {
     {
       type: 'task-time-estimate',
       name: 'longer than 40 hours'
+    },
+
+    /*
+      skills
+    */
+    {
+      type:"skill",
+      name: "3D Design"
+    },
+    {
+      type:"skill",
+      name: "Adobe After Effects"
+    },
+    {
+      type:"skill",
+      name: "Adobe Audition"
+    },
+    {
+      type:"skill",
+      name: "Adobe Flash"
+    },
+    {
+      type:"skill",
+      name: "Adobe Illustrator"
+    },
+    {
+      type:"skill",
+      name: "Adobe Photoshop"
+    },
+    {
+      type:"skill",
+      name: "Android"
+    },
+    {
+      type:"skill",
+      name: "Angular.js"
+    },
+    {
+      type:"skill",
+      name: "API Design"
+    },
+    {
+      type:"skill",
+      name: "Automation"
+    },
+    {
+      type:"skill",
+      name: "Backbone.js"
+    },
+    {
+      type:"skill",
+      name: "Boostrap"
+    },
+    {
+      type:"skill",
+      name: "C"
+    },
+    {
+      type:"skill",
+      name: "C#"
+    },
+    {
+      type:"skill",
+      name: "C++"
+    },
+    {
+      type:"skill",
+      name: "Clustering Technologies"
+    },
+    {
+      type:"skill",
+      name: "CSS"
+    },
+    {
+      type:"skill",
+      name: "Database Management"
+    },
+    {
+      type:"skill",
+      name: "Design for 3D Printing"
+    },
+    {
+      type:"skill",
+      name: "Design for mobile"
+    },
+    {
+      type:"skill",
+      name: "Design for social media (Facebook, Twitter, Instagram)"
+    },
+    {
+      type:"skill",
+      name: "DevOps"
+    },
+    {
+      type:"skill",
+      name: "Django"
+    },
+    {
+      type:"skill",
+      name: "EC2"
+    },
+    {
+      type:"skill",
+      name: "Exhibition/Spatial Design"
+    },
+    {
+      type:"skill",
+      name: "Final Cut Pro"
+    },
+    {
+      type:"skill",
+      name: "HTML"
+    },
+    {
+      type:"skill",
+      name: "HTML5"
+    },
+    {
+      type:"skill",
+      name: "Illustration"
+    },
+    {
+      type:"skill",
+      name: "IOS"
+    },
+    {
+      type:"skill",
+      name: "iWatch"
+    },
+    {
+      type:"skill",
+      name: "Java"
+    },
+    {
+      type:"skill",
+      name: "JavaScript"
+    },
+    {
+      type:"skill",
+      name: "JQuery"
+    },
+    {
+      type:"skill",
+      name: "Logo/Branding"
+    },
+    {
+      type:"skill",
+      name: "Mockups"
+    },
+    {
+      type:"skill",
+      name: "MongoDB"
+    },
+    {
+      type:"skill",
+      name: "Monitoring"
+    },
+    {
+      type:"skill",
+      name: "Motion Graphics"
+    },
+    {
+      type:"skill",
+      name: "mySQL"
+    },
+    {
+      type:"skill",
+      name: "NOC"
+    },
+    {
+      type:"skill",
+      name: "Node.js"
+    },
+    {
+      type:"skill",
+      name: "noSQL"
+    },
+    {
+      type:"skill",
+      name: "Objective-C"
+    },
+    {
+      type:"skill",
+      name: "Packaging Design"
+    },
+    {
+      type:"skill",
+      name: "Photoshop"
+    },
+    {
+      type:"skill",
+      name: "PHP"
+    },
+    {
+      type:"skill",
+      name: "PostrgresSQL"
+    },
+    {
+      type:"skill",
+      name: "Print layout"
+    },
+    {
+      type:"skill",
+      name: "Project Management in Design"
+    },
+    {
+      type:"skill",
+      name: "Prototyping"
+    },
+    {
+      type:"skill",
+      name: "Python"
+    },
+    {
+      type:"skill",
+      name: "Ruby on Rails"
+    },
+    {
+      type:"skill",
+      name: "SQL"
+    },
+    {
+      type:"skill",
+      name: "Storyboarding"
+    },
+    {
+      type:"skill",
+      name: "Swift"
+    },
+    {
+      type:"skill",
+      name: "Testing"
+    },
+    {
+      type:"skill",
+      name: "User Experience Design (UX)"
+    },
+    {
+      type:"skill",
+      name: "User Interface Design (UI)"
+    },
+    {
+      type:"skill",
+      name: "Video"
+    },
+    {
+      type:"skill",
+      name: "Web Design"
+    },
+    {
+      type:"skill",
+      name: "X-Code"
     },
 
   ]
