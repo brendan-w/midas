3c480d5a66cd8ffafd15d088f7c2336ed3efc05a
Brendan Whitfield <brendanw@windworksdesign.com>
2015-08-03 17:03:24 -0400

    removed old code from footer

--- assets/js/backbone/apps/footer/views/footer_view.js
+++ assets/js/backbone/apps/footer/views/footer_view.js
@@ -22,22 +22,16 @@ var FooterView = Backbone.View.extend({
 
     function resizeElements() {
       if ($(window).height() > $('body').height())
-        {
+      {
         var extra = $(window).height() - $('body').height();
         extra /= 2;
         self.$el.css({ "margin-top": extra + "px" });
-        }
+      }
     }
-    //   var px_in_window = $(window).height() - ($(document.body).height() - self.$el.height());
 
-    //   //peg at zero if the footer is already off screen
-    //   px_in_window = Math.max(px_in_window, 0);
-    //   self.$el.css({ "margin-top": px_in_window + "px" });
-    // }
-
-     resizeElements(); //trigger initial size calculation
-     $("#container").bind("DOMSubtreeModified", resizeElements);
-     $(window).bind("resize", resizeElements);
+    resizeElements(); //trigger initial size calculation
+    $("#container").bind("DOMSubtreeModified", resizeElements);
+    $(window).bind("resize", resizeElements);
   },
 
   cleanup: function () {
