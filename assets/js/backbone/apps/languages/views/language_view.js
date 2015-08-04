3529f39b06fda0d0129b0eee8d8fcb603b998592
Brendan Whitfield <brendanw@windworksdesign.com>
2015-08-04 16:04:15 -0400

    added initial_langs property to the render

--- assets/js/backbone/apps/languages/views/language_view.js
+++ assets/js/backbone/apps/languages/views/language_view.js
@@ -14,45 +14,101 @@ var Languages = Backbone.View.extend({
   events: {
     "click .lang .close" : "delete_lang", 
     "click .lang-add"    : "add_lang",
-    "click .lang-test"   : "load_into_user",
+    "click .lang-test"   : "data",
   },
 
   initialize: function() {
     this.tagFactory = new TagFactory();
   },
 
-  render: function(user) {
+  render: function(initial_langs) {
     var self = this;
+    initial_langs = initial_langs || [];
 
     this.$el.html(_.template(LanguageTemplate)({
       //nothing so far...
     }));
 
-    //if a user model is provided, use it to fill the UI
-    if(user)
-    {
-      this.$(".lang-list").append(_.template(LanguageItemTemplate)({
-        id: "",
-      }));
+    //if initial_langs are given, use them to fill the UI
+    this.tagFactory.fetchAllTagsOfType("lang-proficiency", function(tags) {
 
-    }
+      self.proficiencies = tags;
+
+      initial_langs.forEach(function(lang) {
+        self.$(".lang-list").append(_.template(LanguageItemTemplate)({
+          id: lang.id,
+        }));
+
+        var $lang    = self.$(".lang-list").children().last();
+        var $written = $lang.find(".lang-written .lang-proficiency");
+        var $spoken  = $lang.find(".lang-spoken  .lang-proficiency");
+
+        //make the dropdowns
+        self.tagFactory.createTagDropDown({
+          type:        "lang-proficiency",
+          selector:    $written,
+          multiple:    false,
+          allowCreate: false,
+          searchable:  false,
+          width:       "100%",
+          fillWith:    self.proficiencies,
+          data:        _.findWhere(this.proficiencies, { id : lang.written }),
+        });
+
+        self.tagFactory.createTagDropDown({
+          type:        "lang-proficiency",
+          selector:    $spoken,
+          multiple:    false,
+          allowCreate: false,
+          searchable:  false,
+          width:       "100%",
+          fillWith:    self.proficiencies,
+          data:        _.findWhere(this.proficiencies, { id : lang.spoken }),
+        });
+
+      }); //initial_langs.forEach()
+
+    }); //fetchAllTagsOfType()
 
     this.$el.i18n();
 
     return this;
   },
 
-  load_into_user: function(e) {
+  data: function(e) {
     if(e && e.preventDefault) e.preventDefault();
+
+    if(validateAll(this.$el))
+      return false;
+
+    var langs = [];
+
     this.$(".lang").each(function(i, lang) {
       var $lang = $(lang);
+      var id    = $lang.data('id');
 
-      //fetch the proficiencies
-      var written = $lang.find(".lang-written .lang-proficiency").select2('data');
-      var spoken  = $lang.find(".lang-spoken  .lang-proficiency").select2('data');
+      //fetch the language name and the proficiencies
+      var language = $lang.find(".lang-name").val();
+      var written = $lang.find(".lang-written .lang-proficiency").select2('data').id;
+      var spoken  = $lang.find(".lang-spoken  .lang-proficiency").select2('data').id;
 
-
+      if(id)
+      {
+        langs.push(id);
+      }
+      else
+      {
+        langs.push({
+          language:           language,
+          writtenProficiency: written,
+          spokenProficiency:  spoken,
+        });
+      }
     });
+
+    console.log(langs);
+
+    return langs;
   },
 
   add_lang: function(e) {
@@ -61,25 +117,22 @@ var Languages = Backbone.View.extend({
 
     //add a new language item
     this.$(".lang-list").append(_.template(LanguageItemTemplate)({
-      id: "",
+      id: "", //not a pre-existing language
     }));
 
     //select it
     var $items = this.$(".lang-list").children().last().find(".lang-proficiency");
 
     //instantiate the dropdowns
-    this.tagFactory.fetchAllTagsOfType("lang-proficiency", function(tags) {
-      self.tagFactory.createTagDropDown({
-        type:        "lang-proficiency",
-        selector:    $items,
-        placeholder: "Select a proficiency",
-        multiple:    false,
-        allowCreate: false,
-        searchable:  false,
-        width:       "100%",
-        fillWith:    tags,
-        data:        tags[0],
-      });
+    self.tagFactory.createTagDropDown({
+      type:        "lang-proficiency",
+      selector:    $items,
+      multiple:    false,
+      allowCreate: false,
+      searchable:  false,
+      width:       "100%",
+      fillWith:    this.proficiencies,
+      data:        this.proficiencies[0],
     });
 
   },
