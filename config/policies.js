0dd41947153d5144aea48c76a1dc5233cd448071
Brendan Whitfield <brendanw@windworksdesign.com>
2015-08-16 16:05:56 -0400

    alphabetized policies

--- config/policies.js
+++ config/policies.js
@@ -39,6 +39,114 @@ module.exports.policies = {
     'register': ['register']
   },
 
+  AttachmentController: {
+    'find':               ['passport', 'authenticated', 'hasIdParam'],
+    'findOne':            ['passport', 'authenticated', 'hasIdParam'],
+    'findAllByProjectId': [                             'hasIdParam', 'project'],
+    'findAllByTaskId':    [                             'hasIdParam', 'task'],
+    'findAllByUserId':    ['passport', 'authenticated', 'hasIdParam'],
+    'create':             ['passport', 'authenticated', 'attachment'],
+    'update':             false,
+    'destroy':            ['passport', 'authenticated']
+  },
+
+  CommentController : {
+    'find':               false,
+    'findOne':            false,
+    'create':             ['passport', 'authenticated', 'addUserId', 'projectId', 'taskId'],
+    'update':             ['passport', 'authenticated', 'projectId', 'taskId', 'comment', 'ownerOrAdmin'],
+    'destroy':            ['passport', 'authenticated', 'hasIdParam', 'admin'],
+    'findAllByProjectId': ['passport', 'authenticated', 'hasIdParam', 'project'],
+    'findAllByTaskId':    ['passport', 'authenticated', 'hasIdParam', 'task']
+  },
+
+  EventController : {
+    '*': false,
+    'find':               ['passport', 'authenticated'],
+    'findOne':            ['passport', 'authenticated'],
+    'create':             ['passport', 'authenticated', 'addUserId', 'projectId', 'eventUuid'],
+    'update':             ['passport', 'authenticated', 'projectId'],
+    'findAllByProjectId': ['passport', 'authenticated', 'addUserId', 'hasIdParam', 'project'],
+    'attend':             ['passport', 'authenticated', 'addUserId', 'hasIdParam'],
+    'cancel':             ['passport', 'authenticated', 'addUserId', 'hasIdParam'],
+    'rsvp':               ['passport', 'authenticated', 'addUserId'],
+    'ical':               ['passport', 'authenticated', 'addUserId', 'project'],
+    'destroy':            ['passport', 'authenticated', 'hasIdParam', 'admin']
+  },
+
+  // Disable the index blueprints for FileController due to security concerns
+  FileController : {
+    'index':      false,
+    'findAll':    ['admin'],
+
+    // for testing
+    'test':       true,
+    'testupload': true,
+
+    // everything else is protected
+    '*':          ['passport', 'protectedFile']
+  },
+
+  LikeController : {
+    '*':       ['passport', 'authenticated', 'addUserId'],
+    'count':   ['passport', 'authenticated', 'hasIdParam', 'project'],
+    'countt':  ['passport', 'authenticated', 'hasIdParam', 'task'],
+    'countu':  ['passport', 'authenticated', 'hasIdParam'],
+    'like':    ['passport', 'authenticated', 'addUserId', 'hasIdParam'],
+    'liket':   ['passport', 'authenticated', 'addUserId', 'hasIdParam'],
+    'likeu':   ['passport', 'authenticated', 'addUserId', 'hasIdParam'],
+    'unlike':  ['passport', 'authenticated', 'addUserId', 'hasIdParam'],
+    'unliket': ['passport', 'authenticated', 'addUserId', 'hasIdParam'],
+    'unlikeu': ['passport', 'authenticated', 'addUserId', 'hasIdParam'],
+    'create':  ['passport', 'authenticated', 'addUserId'],
+    'destroy': false,
+    'update':  false
+  },
+
+  LocationController : {
+    '*':       ['passport', 'authenticated'],
+    'suggest': true,
+  },
+
+  ProjectController : {
+    '*':       ['passport', 'authenticated', 'addUserId', 'project'],
+    'find':    ['passport',                  'hasIdParam', 'project'],
+    'findOne': ['passport',                  'hasIdParam', 'project'],
+    'findAll': ['passport'],
+    'create':  ['passport', 'authenticated', 'addUserId', 'canCreateProject'],
+    'update':  ['passport', 'authenticated', 'hasIdParam', 'project', 'ownerOrAdmin'],
+    'destroy': ['passport', 'authenticated', 'hasIdParam', 'project', 'ownerOrAdmin']
+  },
+
+  ProjectOwnerController : {
+    '*':       false,
+    'create':  ['passport', 'authenticated', 'projectId'],
+    'destroy': ['passport', 'authenticated', 'hasIdParam']
+  },
+
+  SearchController : {
+    '*': true
+  },
+
+  TagEntityController : {
+    '*':       true,
+    'create':  ['passport', 'authenticated'],
+    // Purely for administrative functions
+    'update':  ['passport', 'authenticated', 'admin'],
+    'destroy': ['passport', 'authenticated', 'admin']
+  },
+
+  TaskController : {
+    'find':               ['passport',                  'task'],
+    'findOne':            ['passport',                  'task'],
+    'findAllByProjectId': ['passport',                  'hasIdParam', 'project'],
+    'copy':               ['passport', 'authenticated', 'addUserId'],
+    'create':             ['passport', 'authenticated', 'addUserId', 'canCreateTask'],
+    'update':             ['passport', 'authenticated', 'hasIdParam', 'projectId', 'task', 'ownerOrAdmin'],
+    'destroy':            ['passport', 'authenticated', 'hasIdParam', 'task', 'ownerOrAdmin'],
+    'export':             ['passport', 'authenticated', 'admin']
+  },
+
   // Limit user controller view to just the /user endpoint
   UserController : {
     '*': false,
@@ -65,120 +173,6 @@ module.exports.policies = {
     'destroy': ['passport', 'authenticated', 'hasIdParam','userSettingIdMatch']
   },
 
-  // Disable the index blueprints for FileController due to security concerns
-  FileController : {
-    'index':      false,
-    'findAll':    ['admin'],
-
-    // for testing
-    'test':       true,
-    'testupload': true,
-
-    // everything else is protected
-    '*':          ['passport', 'protectedFile']
-  },
-
-  ProjectController : {
-    '*':       ['passport', 'authenticated', 'addUserId', 'project'],
-    'find':    ['passport',                  'hasIdParam', 'project'],
-    'findOne': ['passport',                  'hasIdParam', 'project'],
-    'findAll': ['passport'],
-    'create':  ['passport', 'authenticated', 'addUserId', 'canCreateProject'],
-    'update':  ['passport', 'authenticated', 'hasIdParam', 'project', 'ownerOrAdmin'],
-    'destroy': ['passport', 'authenticated', 'hasIdParam', 'project', 'ownerOrAdmin']
-  },
-
-  ProjectOwnerController : {
-    '*':       false,
-    'create':  ['passport', 'authenticated', 'projectId'],
-    'destroy': ['passport', 'authenticated', 'hasIdParam']
-  },
-
-  LikeController : {
-    '*':       ['passport', 'authenticated', 'addUserId'],
-    'count':   ['passport', 'authenticated', 'hasIdParam', 'project'],
-    'countt':  ['passport', 'authenticated', 'hasIdParam', 'task'],
-    'countu':  ['passport', 'authenticated', 'hasIdParam'],
-    'like':    ['passport', 'authenticated', 'addUserId', 'hasIdParam'],
-    'liket':   ['passport', 'authenticated', 'addUserId', 'hasIdParam'],
-    'likeu':   ['passport', 'authenticated', 'addUserId', 'hasIdParam'],
-    'unlike':  ['passport', 'authenticated', 'addUserId', 'hasIdParam'],
-    'unliket': ['passport', 'authenticated', 'addUserId', 'hasIdParam'],
-    'unlikeu': ['passport', 'authenticated', 'addUserId', 'hasIdParam'],
-    'create':  ['passport', 'authenticated', 'addUserId'],
-    'destroy': false,
-    'update':  false
-  },
-
-  LocationController : {
-    '*':       ['passport', 'authenticated'],
-    'suggest': true,
-  },
-
-  VolunteerController : {
-    '*': false,
-    'create':  ['passport', 'authenticated', 'addUserId'],
-    'destroy': ['passport', 'authenticated', 'hasIdParam', 'volunteer', 'ownerOrAdmin'],
-  },
-
-  EventController : {
-    '*': false,
-    'find':               ['passport', 'authenticated'],
-    'findOne':            ['passport', 'authenticated'],
-    'create':             ['passport', 'authenticated', 'addUserId', 'projectId', 'eventUuid'],
-    'update':             ['passport', 'authenticated', 'projectId'],
-    'findAllByProjectId': ['passport', 'authenticated', 'addUserId', 'hasIdParam', 'project'],
-    'attend':             ['passport', 'authenticated', 'addUserId', 'hasIdParam'],
-    'cancel':             ['passport', 'authenticated', 'addUserId', 'hasIdParam'],
-    'rsvp':               ['passport', 'authenticated', 'addUserId'],
-    'ical':               ['passport', 'authenticated', 'addUserId', 'project'],
-    'destroy':            ['passport', 'authenticated', 'hasIdParam', 'admin']
-  },
-
-  CommentController : {
-    'find':               false,
-    'findOne':            false,
-    'create':             ['passport', 'authenticated', 'addUserId', 'projectId', 'taskId'],
-    'update':             ['passport', 'authenticated', 'projectId', 'taskId', 'comment', 'ownerOrAdmin'],
-    'destroy':            ['passport', 'authenticated', 'hasIdParam', 'admin'],
-    'findAllByProjectId': ['passport', 'authenticated', 'hasIdParam', 'project'],
-    'findAllByTaskId':    ['passport', 'authenticated', 'hasIdParam', 'task']
-  },
-
-  TagEntityController : {
-    '*':       true,
-    'create':  ['passport', 'authenticated'],
-    // Purely for administrative functions
-    'update':  ['passport', 'authenticated', 'admin'],
-    'destroy': ['passport', 'authenticated', 'admin']
-  },
-
-  TaskController : {
-    'find':               ['passport',                  'task'],
-    'findOne':            ['passport',                  'task'],
-    'findAllByProjectId': ['passport',                  'hasIdParam', 'project'],
-    'copy':               ['passport', 'authenticated', 'addUserId'],
-    'create':             ['passport', 'authenticated', 'addUserId', 'canCreateTask'],
-    'update':             ['passport', 'authenticated', 'hasIdParam', 'projectId', 'task', 'ownerOrAdmin'],
-    'destroy':            ['passport', 'authenticated', 'hasIdParam', 'task', 'ownerOrAdmin'],
-    'export':             ['passport', 'authenticated', 'admin']
-  },
-
-  AttachmentController: {
-    'find':               ['passport', 'authenticated', 'hasIdParam'],
-    'findOne':            ['passport', 'authenticated', 'hasIdParam'],
-    'findAllByProjectId': [                             'hasIdParam', 'project'],
-    'findAllByTaskId':    [                             'hasIdParam', 'task'],
-    'findAllByUserId':    ['passport', 'authenticated', 'hasIdParam'],
-    'create':             ['passport', 'authenticated', 'attachment'],
-    'update':             false,
-    'destroy':            ['passport', 'authenticated']
-  },
-
-  SearchController : {
-    '*': true
-  },
-
   VetController : {
     '*':              false,
     'create':         ['passport', 'authenticated', 'vetCreate'],
@@ -186,5 +180,11 @@ module.exports.policies = {
     'destroy':        ['passport', 'authenticated', 'canVet'],
     'findOne':        ['passport', 'authenticated', 'canVet'],
     'findAllPending': ['passport', 'authenticated', 'canVet'],
+  },
+
+  VolunteerController : {
+    '*': false,
+    'create':  ['passport', 'authenticated', 'addUserId'],
+    'destroy': ['passport', 'authenticated', 'hasIdParam', 'volunteer', 'ownerOrAdmin'],
   },
 };
